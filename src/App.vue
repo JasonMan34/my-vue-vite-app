3c574671f580aa26986d1f7fb85c83c84e20aa07
איתמר צבי <211736459@idf.il>
2021-10-21 15:48:09 +0300

    Git

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { initDarkTheme } from "./utils/dark-theme";
+
+initDarkTheme();
+</script>
+
+<template>
+  <div class="min-h-screen bg:white dark:bg-gray-800">
+    <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  </div>
+</template>
---
+++ src/utils/dark-theme.ts
@@ -0,0 +1,12 @@
+export const initDarkTheme = () => {
+  // Enable dark theme only if specifically stated, or in OS preferences
+  if (
+    localStorage.theme === "dark" ||
+    (!("theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+};
