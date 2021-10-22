2c99a8870f09ad21c0b271ff7819fce57e9c4601
איתמר צבי <211736459@idf.il>
2021-10-22 14:19:49 +0300

    eslint

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
-import { createApp } from 'vue';
-import App from './App.vue';
 import './tailwind.css';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { createApp } from 'vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
+import App from './App.vue';
 
 library.add(faSun, faMoon);
 
 const app = createApp(App);
-app.component('FAIcon', FontAwesomeIcon);
 app.mount('#app');
