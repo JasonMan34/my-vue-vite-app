a124950e3cab7c026dcc7b05ee52482435830df1
JasonMan34 <JasonErlMan@Gmail.com>
2022-02-17 14:34:05 +0200

    Update metric

--- src/components/Minesweeper/game/metric.ts
+++ src/components/Minesweeper/game/metric.ts
@@ -4,8 +4,7 @@ import { MinesweeperGame } from './minesweeper-game';
 const HEIGHT = 16;
 const WIDTH = 30;
 const MINE_COUNT = 99;
-const TestAutoPlayer = async (totalTime: number) => {
-  let runs = 0;
+const TestAutoPlayer = async (runs = 100) => {
   let wins = 0;
   let time = 0;
   const start = new Date();
@@ -14,9 +13,8 @@ const TestAutoPlayer = async (totalTime: number) => {
     wins++;
   };
 
-  while (time < totalTime) {
+  for (let i = 0; i < runs; i++) {
     time = new Date().getTime() - start.getTime();
-    runs++;
     const game = new MinesweeperGame(WIDTH, HEIGHT, MINE_COUNT);
     const player = new AutoPlayer(game);
 
@@ -26,38 +24,40 @@ const TestAutoPlayer = async (totalTime: number) => {
     await player.autoPlay();
   }
 
-  const data = { runs, wins, time, winRate: (runs / wins).toFixed(2) };
-  console.log(data);
+  const data = {
+    runs,
+    wins,
+    time,
+    winRate: (wins * 100) / runs,
+  };
   return data;
 };
 
 const main = async () => {
-  const count = 10;
+  const count = 20;
   let runs = 0;
+  const gameSets = [];
   for (let i = 0; i < count; i++) {
     // eslint-disable-next-line no-await-in-loop
-    const data = await TestAutoPlayer(1000);
+    const data = await TestAutoPlayer(100);
+    gameSets.push(data);
     runs += data.runs;
   }
 
-  console.log(
-    `Average amount of runs in 1 second - ${(runs / count).toFixed(2)}`
-  );
+  const winRates = gameSets.map(a => a.winRate);
+  const highestWinRate = Math.max(...winRates);
+  const lowestWinRate = Math.min(...winRates);
+  const averageWinRate = (
+    winRates.reduce((total, curr) => total + curr, 0) / gameSets.length
+  ).toFixed(2);
 
-  console.log('\n Now we will do the lastIndex improvement');
+  console.log(`Minesweeper bot played ${gameSets.length} sets of 100 games each.
+Its highest win rate was ${highestWinRate}%
+Its lowest win rate was ${lowestWinRate}%
+Its average win rate was ${averageWinRate}%
 
-  (globalThis as any).TEST = true;
-
-  runs = 0;
-  for (let i = 0; i < count; i++) {
-    // eslint-disable-next-line no-await-in-loop
-    const data = await TestAutoPlayer(1000);
-    runs += data.runs;
-  }
-
-  console.log(
-    `Average amount of runs in 1 second - ${(runs / count).toFixed(2)}`
-  );
+The win rates are: ${JSON.stringify(winRates)}
+`);
 };
 
 main();
