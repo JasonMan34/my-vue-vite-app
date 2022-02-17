b40fe6c00eda184838a44e4fdccca1a0b5595950
JasonMan34 <JasonErlMan@Gmail.com>
2022-02-17 18:57:28 +0200

    Mobile friendly

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@ import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
-  <div class="min-h-screen flex flex-col dark-compliant">
+  <div
+    class="min-h-screen min-w-full w-[max-content] flex flex-col dark-compliant"
+  >
     <Navbar />
     <router-view class="flex-1" />
   </div>
