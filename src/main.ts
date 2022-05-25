622c60489f7bf37860913016f9f8da291d992f3e
JasonMan34 <JasonErlMan@Gmail.com>
2022-05-25 19:26:53 +0300

    asd

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,8 @@ import './tailwind.css';
 import 'chart.js/auto';
 import { createApp } from 'vue';
 import { createI18n } from 'vue-i18n';
-import cloneDeep from 'lodash.clonedeep';
 import App from './App.vue';
 import router from './router';
-import type { DataNode } from './components/Minesweeper/game/information';
 
 const i18n = createI18n({ legacy: false, fallbackLocale: 'en-US' });
 const app = createApp(App);
@@ -13,20 +11,3 @@ const app = createApp(App);
 app.use(i18n);
 app.use(router);
 app.mount('#app');
-
-// TODO: Remove these
-
-(globalThis as any).log = (data: any) => {
-  console.log(cloneDeep(data));
-};
-
-(globalThis as any).logInfo = () => {
-  const data = (globalThis as any).info?.data as DataNode[];
-
-  data?.forEach(node => {
-    console.log({
-      ...node.mines,
-      tiles: JSON.stringify(node.tiles.map(a => ({ row: a.row, col: a.col }))),
-    });
-  });
-};
